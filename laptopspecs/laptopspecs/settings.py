91479d547728949f93e298a9e066af65059776c1
Huan Ho (Bruce) <37372806+bruceho293@users.noreply.github.com>
2022-04-12 13:29:20 -0400

    Update settings.py with logging

--- laptopspecs/laptopspecs/settings.py
+++ laptopspecs/laptopspecs/settings.py
@@ -133,3 +133,21 @@ STATICFILES_DIRS = [
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Logging
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
