beebd1ba30f2fc6b6665ff9a47d72fea46665001
Huan Ho (Bruce) <37372806+bruceho293@users.noreply.github.com>
2022-04-12 12:34:58 -0400

    Initial Commit

--- backend/laptopspecs/laptopspecs/settings.py
+++ backend/laptopspecs/laptopspecs/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', 'ltxp.herokuapp.com']
 
 
 # Application definition
