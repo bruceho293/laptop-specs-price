2eefe636380356ee30504cc52ac077b694f89b24
Huan Ho (Bruce) <37372806+bruceho293@users.noreply.github.com>
2022-04-12 13:43:35 -0400

    Update database config

--- laptopspecs/laptopspecs/settings.py
+++ laptopspecs/laptopspecs/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'laptopspecs.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USERNAME"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
     }
 }
 
