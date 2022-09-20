2314fe21f5d1c54283fa0aa4501e4dec3b58fb0b
Huan Ho (Bruce) <37372806+bruceho293@users.noreply.github.com>
2022-09-20 10:25:06 -0400

    Update Hosting Information

--- laptopspecs/laptopspecs/settings.py
+++ laptopspecs/laptopspecs/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', 'ltxp.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'ltxp.herokuapp.com', '18.217.135.159']
 
 
 # Application definition
