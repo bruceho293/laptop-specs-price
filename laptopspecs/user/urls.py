d71e0fe790eb1f6e8b2c1c5c825cada9348660af
Huan Ho (Bruce) <37372806+bruceho293@users.noreply.github.com>
2022-11-18 15:25:07 -0500

    Minor updates on user login

--- laptopspecs/user/urls.py
+++ laptopspecs/user/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('detail/<str:username>/', views.UserDetail.as_view(), name='user-detail'),
     path('register/', views.UserRegister.as_view(), name='user-register'),
-    # path('login/',),
+    path('login/', views.UserLogin.as_view(), name='user-login'),
     # path('logout/'),
 ]
--- laptopspecs/user/views.py
+++ laptopspecs/user/views.py
@@ -1,11 +1,12 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.contrib.auth import authenticate
 
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.permissions import AllowAny, IsAuthenticated
-from rest_framework.decorators import api_view, permission_classes, authentication_classes
-from rest_framework.authentication import BasicAuthentication
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from oauth2_provider.contrib.rest_framework import TokenHasReadWriteScope
+
+import json
 
 from user.serializers import UserProfileDetailSerializer, UserProfileRegisterSerializer
 from user.models import UserProfile
@@ -19,6 +20,36 @@ class UserDetail(generics.RetrieveAPIView):
     lookup_url_kwarg = 'username'
 
 class UserRegister(generics.CreateAPIView):
-    authentication_classes = [BasicAuthentication]
     permission_classes = [AllowAny]
     serializer_class = UserProfileRegisterSerializer
+
+class UserLogin(APIView):
+    permission_classes = [AllowAny]
+    
+    def post(self, request, format=None):
+        """
+        Login request with username and password.
+        With successful login, the client should have get a response.
+        The response should have the access token and refresh token.
+        """
+        
+        # Get username and password.
+        data = json.loads(request.body)
+        username = data.get('username')
+        password = data.get('password')
+
+        # Check if the username exists.
+        if not UserProfile.objects.prefetch_related('user').filter(user__username=username).exists():
+            return Response(data="username \'{}\' does not exists.", status=status.HTTP_404_NOT_FOUND)
+
+        user = authenticate(request=request, username=username, password=password)
+        
+        if user is not None:
+            # Successful Login.
+            # Start Oauth2 Authorization.
+            pass
+        else:
+            return Response(data="Invalid user credentials !", status=status.HTTP_401_UNAUTHORIZED)
+        
+
+
