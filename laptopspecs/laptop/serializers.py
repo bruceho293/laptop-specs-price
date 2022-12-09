b1f2b12ec07f18d16f60a7f58dc5fb08d7389e83
Huan Ho (Bruce) <37372806+bruceho293@users.noreply.github.com>
2022-12-09 16:07:20 -0500

    Refractor and update codes for views and serializers for Laptop.

--- laptopspecs/laptop/serializers.py
+++ laptopspecs/laptop/serializers.py
@@ -1,6 +1,8 @@
+import math
 from rest_framework import serializers
 
 from laptop.models import Laptop, Memo, Component
+from laptop.utils import get_closest_components_price
 from user.models import UserImpression
 
 class BaseUserImpressionSerializer(serializers.Serializer):
@@ -15,6 +17,23 @@ class BaseUserImpressionSerializer(serializers.Serializer):
     
     def get_dislike_count(self, obj):
         return UserImpression.objects.filter(laptop=obj, liked=False).count()
+
+class BaseLaptopMethodFieldSerializer(serializers.Serializer):
+    price_compare = serializers.SerializerMethodField()
+
+    class Meta:
+        fields = ['price_compare']
+
+    def get_price_compare(self, obj):
+        # `obj`is a Laptop obj.
+        # TODO: Find a way to get the price comparision. CURRENT: Too heavy on the database query.
+        
+        # laptop_price = float(obj.price)
+        # closest_components_total_price = get_closest_components_price(laptop=obj)
+        # return math.ceil(closest_components_total_price*100 / 100) - laptop_price
+        
+        # Return the laptop price for now.
+        return obj.price
 
 class ComponentSerializer(serializers.ModelSerializer):
     brand_name = serializers.ReadOnlyField(source='brand.name')
@@ -36,16 +55,18 @@ class MemoSerializer(serializers.ModelSerializer):
         model = Memo
         fields = ['name', 'category']
 
-class LaptopListSerializer(BaseUserImpressionSerializer, serializers.ModelSerializer):
+class LaptopListSerializer(BaseLaptopMethodFieldSerializer, BaseUserImpressionSerializer, serializers.ModelSerializer):
     class Meta:
         model = Laptop
         fields = ['name', 'slug', 'price', 'updated'] + \
+            BaseLaptopMethodFieldSerializer.Meta.fields + \
             BaseUserImpressionSerializer.Meta.fields
 
-class LaptopDetailSerializer(BaseUserImpressionSerializer, serializers.ModelSerializer):
+class LaptopDetailSerializer(BaseLaptopMethodFieldSerializer, BaseUserImpressionSerializer, serializers.ModelSerializer):
     brand_name = serializers.ReadOnlyField(source='brand.name')
     specs = MemoSerializer(many=True, read_only=True)
     class Meta:
         model = Laptop
         fields = ['name', 'slug', 'brand_name', 'price', 'link', 'updated', 'specs'] + \
+            BaseLaptopMethodFieldSerializer.Meta.fields + \
             BaseUserImpressionSerializer.Meta.fields
---
+++ laptopspecs/laptop/utils.py
@@ -0,0 +1,74 @@
+from django.db.models import F
+
+from laptop.models import Laptop, Component, Memo
+from laptop.managers import ComponentType
+
+def get_memo_with_component_qnty(laptop: Laptop):
+    """
+    Get the dictionary of `Memo` objects describling the name of laptop components in the specification.
+    
+    :param laptop: `Laptop` object.
+    :return: a tuple of  `Memo` QuerySet for each component type. 
+    """
+
+    memos = Memo.objects.filter(note_detail__laptop=laptop).annotate(qnty=F('note_detail__qnty')).order_by('name')
+    cpu = memos.filter(category=ComponentType.CPU)  
+    ram = memos.filter(category=ComponentType.RAM)
+    gpu = memos.filter(category=ComponentType.GPU)
+    disk = memos.filter(category=ComponentType.DISK)
+
+    return cpu, ram, gpu, disk
+    
+
+def get_closest_components(laptop: Laptop=None, qnty_memo=None):
+    """
+    Get the closest matching components based on the specification of the laptop.
+    If `qnty_memo` is used, use `qnty_memo` instead.
+    
+    :param laptop: `Laptop` object.
+    :return closest_component: a dict with component type Key and `Component` object Value. 
+    """
+    cpu, ram, gpu, disk = None, None, None, None
+
+    if qnty_memo is not None:
+        cpu, ram, gpu, disk = qnty_memo
+    else:
+        cpu, ram, gpu, disk = get_memo_with_component_qnty(laptop)
+    
+    # Get the matching components.
+    # Calculate the total price of the components.
+    closest_component = {
+        'processor': Component.category_manager.get_closest_processor(cpu),
+        'memory': Component.category_manager.get_closest_memory_chip(ram),
+        'graphics_card': Component.category_manager.get_closest_graphics_card(gpu),
+        'storage': Component.category_manager.get_closest_storage(disk) 
+    }
+
+    return closest_component
+
+def get_closest_components_price(laptop:Laptop = None, closest_components: dict = None):
+    """
+    Get the total price of the matching component based on the laptop.
+    If both `laptop` and `closest_component` args is supplied, ignore `laptop` and calculate price based on `closest_component`. 
+    
+    :param laptop: `Laptop` object.
+    :param closest_component:  a dict with component type Key and `Component` object Value. 
+    :return: the total price of the closest matching components.
+    """
+    
+    if laptop is None and closest_components is None:
+        return 0
+    elif laptop is not None and closest_components is None:
+        closest_components = get_closest_components(laptop)
+   
+    # If both 'laptop' and 'closest_component' args is supplied,
+    # ignore 'laptop' and calculate the total price of 'closest_component' 
+    total_comps_price = 0
+    for category_comp in closest_components.keys():
+        components = closest_components[category_comp]
+        if components.exists():
+            for component in components:
+                count = component.comp_count
+                price = component.get_price
+                total_comps_price += float(price) * count
+    return total_comps_price
