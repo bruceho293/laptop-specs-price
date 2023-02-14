e4c52eab442787ec579f890db80e6d721cec70a8
Huan Ho (Bruce) <37372806+bruceho293@users.noreply.github.com>
2023-02-14 12:29:15 -0500

    Modified user credential decorator

--- laptopspecs/user/decorators.py
+++ laptopspecs/user/decorators.py
@@ -1,8 +1,10 @@
 import base64
 import os
+
+from dotenv import load_dotenv
 from functools import wraps
 
-from django.contrib.auth.models import User
+load_dotenv()
 
 def get_client_credentials(view_func):
     """
@@ -11,7 +13,7 @@ def get_client_credentials(view_func):
     @wraps(view_func)
     def _wrapper_view(request, *args, **kwargs):
         client_id = request.POST.get('client_id')
-        client_secret = os.environ.get('OAUTH2_CLIENT_SECRET')
+        client_secret = os.getenv('OAUTH2_CLIENT_SECRET')
         client = base64.b64encode('{}:{}'.format(client_id, client_secret).encode()).decode()    
         request.META['HTTP_AUTHORIZATION'] = 'Basic {}'.format(client)
         return view_func(request, *args, **kwargs)
